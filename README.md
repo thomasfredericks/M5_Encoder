0eededd7d5759dd9048aadb846ae32143bf0040d
Thomas O Fredericks <thomas@fredericks.info>
2025-11-17 16:05:11 -0500

    Initialisation

---
+++ README.md
@@ -0,0 +1,11 @@
+# M5_Encoder
+
+## Overview
+
+Simple library for the [Encoder Unit | m5stack-store](https://shop.m5stack.com/products/encoder-unit).
+
+## Description
+
+This is a library for the M5Stack Unit Encoder (SKU: U135). It allows you to easily interface with the encoder and read its position and button state or set its pixels.
+
+It is based on [m5stack/M5Unit-Encoder: Contains case programs of M5Stack Unit Encoder.](https://github.com/m5stack/M5Unit-Encoder)
---
+++ src/M5_Encoder.h
@@ -0,0 +1,27 @@
+
+#ifndef _UNIT_ENCODER_H_
+#define _UNIT_ENCODER_H_
+
+#include "Arduino.h"
+
+
+class Unit_Encoder {
+private:
+    uint8_t _addr;
+    TwoWire* _wire;
+    uint8_t _scl;
+    uint8_t _sda;
+    uint32_t _speed;
+    void writeBytes(uint8_t addr, uint8_t reg, uint8_t* buffer, uint8_t length);
+    void readBytes(uint8_t addr, uint8_t reg, uint8_t* buffer, uint8_t length);
+
+public:
+    void begin(TwoWire* wire = &Wire, uint8_t addr = ENCODER_ADDR, uint8_t sda = SDA, uint8_t scl = SCL,
+               uint32_t speed = 200000L);
+    signed short int getEncoderValue();
+    bool getButtonStatus();
+    void setLEDColor(uint8_t index, uint32_t color);
+    void setWorkMode(uint8_t mode);
+};
+
+#endif
